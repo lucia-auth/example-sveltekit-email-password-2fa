f0410afef0f8585fac85968807a87e9076766549
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2024-10-03 18:40:24 +0900

    update layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <svelte:head>
 	<title>Email and password example with 2FA in SvelteKit</title>
 </svelte:head>
+<header>
+	<a href="/">Home</a>
+	<a href="/settings">Settings</a>
+</header>
 <main>
 	<slot />
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,13 +6,7 @@
 	export let data: PageData;
 </script>
 
-<header>
-	<a href="/">Home</a>
-	<a href="/settings">Settings</a>
-</header>
-<main>
-	<h1>Hi {data.user.username}!</h1>
-	<form method="post" use:enhance>
-		<button>Sign out</button>
-	</form>
-</main>
+<h1>Hi {data.user.username}!</h1>
+<form method="post" use:enhance>
+	<button>Sign out</button>
+</form>
